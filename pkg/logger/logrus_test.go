f051585cab217e98faf7413043e53d6831fb630d
Arun Gopalpuri <arun@gopalpuri.com>
2020-12-05 15:16:08 -0800

    adding more tests

--- pkg/logger/logrus_test.go
+++ pkg/logger/logrus_test.go
@@ -19,13 +19,120 @@ func TestLogrusInfoLogger(t *testing.T) {
 	absLogger, _ := NewLogrusLogger(logrus)
 	absLogger.WithFields(Fields{
 		"foo": "bar",
-	}).Infof("direct")
+	}).Info("direct")
 
 	err := json.Unmarshal(buffer.Bytes(), &fields)
 	assert.Nil(t, err)
 	assert.Equal(t, "direct", fields["msg"])
 	assert.Equal(t, "info", fields["level"])
 	assert.Equal(t, "bar", fields["foo"])
+}
+
+func TestLogrusInfofLogger(t *testing.T) {
+	var fields Fields
+	var buffer bytes.Buffer
+	logrus := log.New()
+	logrus.SetFormatter(&log.JSONFormatter{})
+	logrus.SetOutput(&buffer)
+	logrus.SetLevel(log.DebugLevel)
+	absLogger, _ := NewLogrusLogger(logrus)
+	absLogger.WithFields(Fields{
+		"ping": "pong",
+	}).Infof("received %s balls", "ping pong")
+
+	err := json.Unmarshal(buffer.Bytes(), &fields)
+	assert.Nil(t, err)
+	assert.Equal(t, "received ping pong balls", fields["msg"])
+	assert.Equal(t, "info", fields["level"])
+	assert.Equal(t, "pong", fields["ping"])
+}
+
+func TestLogrusWarnLogger(t *testing.T) {
+	var fields Fields
+	var buffer bytes.Buffer
+	logrus := log.New()
+	logrus.SetFormatter(&log.JSONFormatter{})
+	logrus.SetOutput(&buffer)
+	logrus.SetLevel(log.DebugLevel)
+	absLogger, _ := NewLogrusLogger(logrus)
+	absLogger.WithFields(Fields{
+		"foo": "bar",
+		"log": "logrus",
+	}).Warn("direct")
+
+	err := json.Unmarshal(buffer.Bytes(), &fields)
+	assert.Nil(t, err)
+	assert.Equal(t, "direct", fields["msg"])
+	assert.Equal(t, "warning", fields["level"])
+	assert.Equal(t, "bar", fields["foo"])
+	assert.Equal(t, "logrus", fields["log"])
+}
+
+func TestLogrusWarnfLogger(t *testing.T) {
+	var fields Fields
+	var buffer bytes.Buffer
+	logrus := log.New()
+	logrus.SetFormatter(&log.JSONFormatter{})
+	logrus.SetOutput(&buffer)
+	logrus.SetLevel(log.DebugLevel)
+	absLogger, _ := NewLogrusLogger(logrus)
+	absLogger.WithFields(Fields{
+		"ping": "pong",
+		"log":  "logrus",
+	}).Warnf("received %s balls", "table tennis")
+
+	err := json.Unmarshal(buffer.Bytes(), &fields)
+	assert.Nil(t, err)
+	assert.Equal(t, "received table tennis balls", fields["msg"])
+	assert.Equal(t, "warning", fields["level"])
+	assert.Equal(t, "pong", fields["ping"])
+	assert.Equal(t, "logrus", fields["log"])
+}
+
+func TestLogrusPanicLogger(t *testing.T) {
+	var fields Fields
+	var buffer bytes.Buffer
+	logrus := log.New()
+	logrus.SetFormatter(&log.JSONFormatter{})
+	logrus.SetOutput(&buffer)
+	logrus.SetLevel(log.ErrorLevel)
+	absLogger, _ := NewLogrusLogger(logrus)
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+		err := json.Unmarshal(buffer.Bytes(), &fields)
+		assert.Nil(t, err)
+		assert.Equal(t, "db not found", fields["msg"])
+		assert.Equal(t, "panic", fields["level"])
+		assert.Equal(t, "dataDB", fields["db"])
+		assert.Equal(t, "logrus", fields["log"])
+	}()
+	absLogger.WithFields(Fields{
+		"db":  "dataDB",
+		"log": "logrus",
+	}).Panic("db not found")
+}
+
+func TestLogursErrorLogger(t *testing.T) {
+	var fields Fields
+	var buffer bytes.Buffer
+	logrus := log.New()
+	logrus.SetFormatter(&log.JSONFormatter{})
+	logrus.SetOutput(&buffer)
+	logrus.SetLevel(log.DebugLevel)
+	absLogger, _ := NewLogrusLogger(logrus)
+	absLogger.WithFields(Fields{
+		"acctNumber": 7899,
+		"log":        "logrus",
+	}).Errorf("Error creating account %s", "testAccount")
+
+	err := json.Unmarshal(buffer.Bytes(), &fields)
+	assert.Nil(t, err)
+	assert.Equal(t, "Error creating account testAccount", fields["msg"])
+	assert.Equal(t, "error", fields["level"])
+	assert.Equal(t, float64(7899), fields["acctNumber"])
+	assert.Equal(t, "logrus", fields["log"])
 }
 
 //set logger to info and see that it doesn't print debug statements
--- pkg/logger/zap_test.go
+++ pkg/logger/zap_test.go
@@ -24,13 +24,140 @@ func TestZapInfoLogger(t *testing.T) {
 	absLogger, _ := NewZapLogger(z)
 	absLogger.WithFields(Fields{
 		"foo": "bar",
-	}).Infof("direct")
+	}).Info("direct")
 
 	err := json.Unmarshal(buffer.Bytes(), &fields)
 	assert.Nil(t, err)
 	assert.Equal(t, "direct", fields["msg"])
 	assert.Equal(t, "info", fields["level"])
 	assert.Equal(t, "bar", fields["foo"])
+}
+
+func TestZapInfofLogger(t *testing.T) {
+	var fields Fields
+	var buffer bytes.Buffer
+	writerSync := zapcore.AddSync(&buffer)
+	encoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	core := zapcore.NewCore(
+		encoder,
+		writerSync,
+		zap.DebugLevel,
+	)
+	z := zap.New(core)
+	absLogger, _ := NewZapLogger(z)
+	absLogger.WithFields(Fields{
+		"ping": "pong",
+	}).Infof("received %s balls", "ping pong")
+
+	err := json.Unmarshal(buffer.Bytes(), &fields)
+	assert.Nil(t, err)
+	assert.Equal(t, "received ping pong balls", fields["msg"])
+	assert.Equal(t, "info", fields["level"])
+	assert.Equal(t, "pong", fields["ping"])
+}
+
+func TestZapWarnLogger(t *testing.T) {
+	var fields Fields
+	var buffer bytes.Buffer
+	writerSync := zapcore.AddSync(&buffer)
+	encoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	core := zapcore.NewCore(
+		encoder,
+		writerSync,
+		zap.DebugLevel,
+	)
+	z := zap.New(core)
+	absLogger, _ := NewZapLogger(z)
+	absLogger.WithFields(Fields{
+		"foo": "bar",
+		"log": "zap",
+	}).Warn("direct")
+
+	err := json.Unmarshal(buffer.Bytes(), &fields)
+	assert.Nil(t, err)
+	assert.Equal(t, "direct", fields["msg"])
+	assert.Equal(t, "warn", fields["level"])
+	assert.Equal(t, "bar", fields["foo"])
+	assert.Equal(t, "zap", fields["log"])
+}
+
+func TestZapWarnfLogger(t *testing.T) {
+	var fields Fields
+	var buffer bytes.Buffer
+	writerSync := zapcore.AddSync(&buffer)
+	encoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	core := zapcore.NewCore(
+		encoder,
+		writerSync,
+		zap.DebugLevel,
+	)
+	z := zap.New(core)
+	absLogger, _ := NewZapLogger(z)
+	absLogger.WithFields(Fields{
+		"ping": "pong",
+		"log":  "zap",
+	}).Warnf("received %s balls", "table tennis")
+
+	err := json.Unmarshal(buffer.Bytes(), &fields)
+	assert.Nil(t, err)
+	assert.Equal(t, "received table tennis balls", fields["msg"])
+	assert.Equal(t, "warn", fields["level"])
+	assert.Equal(t, "pong", fields["ping"])
+	assert.Equal(t, "zap", fields["log"])
+}
+
+func TestZapPanicLogger(t *testing.T) {
+	var fields Fields
+	var buffer bytes.Buffer
+	writerSync := zapcore.AddSync(&buffer)
+	encoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	core := zapcore.NewCore(
+		encoder,
+		writerSync,
+		zap.ErrorLevel,
+	)
+	z := zap.New(core)
+	absLogger, _ := NewZapLogger(z)
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+		err := json.Unmarshal(buffer.Bytes(), &fields)
+		assert.Nil(t, err)
+		assert.Equal(t, "db not found", fields["msg"])
+		assert.Equal(t, "panic", fields["level"])
+		assert.Equal(t, "dataDB", fields["db"])
+		assert.Equal(t, "zap", fields["log"])
+	}()
+	absLogger.WithFields(Fields{
+		"db":  "dataDB",
+		"log": "zap",
+	}).Panic("db not found")
+}
+
+func TestZapErrorLogger(t *testing.T) {
+	var fields Fields
+	var buffer bytes.Buffer
+	writerSync := zapcore.AddSync(&buffer)
+	encoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	core := zapcore.NewCore(
+		encoder,
+		writerSync,
+		zap.InfoLevel,
+	)
+	z := zap.New(core)
+	absLogger, _ := NewZapLogger(z)
+	absLogger.WithFields(Fields{
+		"acctNumber": 7899,
+		"log":        "zap",
+	}).Errorf("Error creating account %s", "testAccount")
+
+	err := json.Unmarshal(buffer.Bytes(), &fields)
+	assert.Nil(t, err)
+	assert.Equal(t, "Error creating account testAccount", fields["msg"])
+	assert.Equal(t, "error", fields["level"])
+	assert.Equal(t, float64(7899), fields["acctNumber"])
+	assert.Equal(t, "zap", fields["log"])
 }
 
 //set logger to info and see that it doesn't print debug statements
